c3beafa99b52bde83bf0262b0854bf4e43e86765
joshleewillis <joshleewillis@gmail.com>
2025-05-30 13:10:03 -0500

    Add fa down arrow to header link.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -59,7 +59,10 @@ export default function Header() {
         <div id="intro">
           <h1 id="intro-title">Hi, I'm <u>Josh Willis</u>.</h1>
           <p id="intro-text">A passionate <b>Web Developer</b> crafting clean, responsive, and user-friendly websites and applications.</p>
-          <a href="#project-grid-container" id="intro-link">Check out my work below!</a>
+          <a href="#project-grid-container" id="intro-link">
+            Check out my work below!
+            <i className="fas fa-arrow-down arrow-icon"></i>
+          </a>
         </div>
 
         <hr className="rounded" />
