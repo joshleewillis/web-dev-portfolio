0aea2ddfcbba0c9c3e432e783fe80f7974e48134
joshleewillis <99991921+joshleewillis@users.noreply.github.com>
2025-08-14 12:39:01 -0500

    Add first site owner photo to intro section.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -63,6 +63,7 @@ export default function Header() {
             Check out my work below!
             <i className="fas fa-arrow-down arrow-icon"></i>
           </a>
+          <img src="./assets/ThoughtfulPhoto.jpg" className="site-owner-photo" alt="First Photo of Site Owner" />
         </div>
 
         <hr className="rounded" />
