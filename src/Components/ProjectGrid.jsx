3d32db7fbdb5717c975b54c27ff15f6aa2617196
joshleewillis <99991921+joshleewillis@users.noreply.github.com>
2025-09-23 07:35:32 -0500

    Place show-hide-project-list-button inside its own container.

--- src/Components/ProjectGrid.jsx
+++ src/Components/ProjectGrid.jsx
@@ -153,9 +153,11 @@ export default function ProjectGrid() {
             ))
           } 
         </div>
-        <button onClick={() => setShowAllTiles(!showAllTiles)} className="show-hide-project-list-button">
-          {showAllTiles ? 'Hide Projects' : 'Show All Projects'}
-        </button>
+        <div className="show-hide-project-list-button-container">
+          <button onClick={() => setShowAllTiles(!showAllTiles)} className="show-hide-project-list-button">
+            {showAllTiles ? 'Hide Projects' : 'Show All Projects'}
+          </button>
+        </div>
       </section>
     </main>
   );
